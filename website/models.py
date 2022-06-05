275fba3271a430ed837ef4535e49289eb84a401e
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-06-05 20:02:47 -0300

    project startup

---
+++ website/models.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+from . import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
