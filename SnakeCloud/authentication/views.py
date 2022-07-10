cc9337f8017404919f699165e1f28a6b6e210664
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-10 12:13:05 -0300

    starting with django

---
+++ SnakeCloud/authentication/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+
+def logout(request):
+    ...
