5d47c3a2c437a3cdd08ae73b18c64d862c5030e9
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-12 19:52:42 -0300

    JOF upload template

--- SnakeCloud/jungle_of_files/views.py
+++ SnakeCloud/jungle_of_files/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='/auth/login/')
 def dashboard(request):
     return render(request, 'jungle/dashboard.html')
+
+
+@login_required(login_url='/auth/login/')
+def upload(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        return HttpResponse(f'{title} {description}')
+    elif request.method == 'GET':
+        return render(request, 'jungle/upload.html')
+
