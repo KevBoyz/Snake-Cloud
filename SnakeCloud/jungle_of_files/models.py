4daf6f5bf4e3d241bc7f79b362a8f23bad50df40
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-12 22:04:52 -0300

    Uploading files now

--- SnakeCloud/jungle_of_files/models.py
+++ SnakeCloud/jungle_of_files/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FilePost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    description = models.TextField()
+    file = models.FileField(upload_to='jof')
+    # image_post = models.FileField(upload_to='jof/img')
+
+    def __str__(self):
+        return self.title
--- SnakeCloud/jungle_of_files/views.py
+++ SnakeCloud/jungle_of_files/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import FilePost
 
 
 @login_required(login_url='/auth/login/')
@@ -12,7 +13,18 @@ def upload(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
-        return HttpResponse(f'{title} {description}')
+        file = request.FILES.get('upload_file')
+        try:
+            file_upload = FilePost(
+                user=request.user,
+                title=title,
+                description=description,
+                file=file
+            )
+            file_upload.save()
+            return redirect('dashboard')
+        except Exception as e:
+            return HttpResponse(e)
     elif request.method == 'GET':
         return render(request, 'jungle/upload.html')
 
